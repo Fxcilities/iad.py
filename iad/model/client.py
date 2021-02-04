a49682ce9f67262d7a78e61f7814ecb203ee1b75
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-02-04 09:59:29 +0700

    major improvements to iad.py's code

--- iad/model/client.py
+++ iad/model/client.py
@@ -1,84 +1,67 @@
-import asyncio
 from datetime import datetime
 from io import BytesIO
-from typing import Tuple
-
-import PIL
 from aiohttp import ClientSession
-
-from ..errors import (InvalidContentType, InvalidToken, JsonDecodeException,
-                      RequestError)
+from os import path
+from typing import Tuple
+from ..errors import JsonDecodeException, RequestError
 from ..ratelimit import RateLimiter
 from ..request import Request
 from .upload import Upload
 
-
 class ImADev:
-
     DISABLED = ('php', 'html', 'js', 'css', 'ts')
+    def __init__(self, token, session = None):
+        assert isinstance(token, str), f"token argument expected str, got {token.__class__.__name__}"
+        self.__token = token
+        self._http = RateLimiter(session or ClientSession())
 
-    def __init__(self, token: str = None):
+    def __repr__(self):
+        return "<ImADev object>"
 
-        self.token = token
-        self._http = ClientSession()
-        self._http = RateLimiter(self._http)
-
-        if self.token is None:
-            raise ValueError('No token specified')
-        elif type(self.token) != str:
-            raise ValueError('token argument not str, got {}'.format(self.token.__class__.__name__))
-
-    async def upload(self, data: Tuple[str, bytes] = None):
-        file = data[1]
-        if data is None:
-            raise ValueError('file argument not specified')
-        if type(file) != bytes:
-            raise ValueError('file argument not bytes, got {}'.format(file.__class__.__name__))
-
-        if(data[0] in self.DISABLED):
-            raise InvalidContentType('{} is a API blacklisted file extension.'.format(data[0]))
+    async def upload(self, data, file_format: str = None):
+        if isinstance(data, str):
+            assert isinstance(data, str), "file_format argument not specified"
+            assert path.isfile(data), f"File not found: {data}"
+            if (not file_format) or (not isinstance(file_format, str)): # if file format is not specified, we try to find it from the path
+                file_format = data.strip("\\").strip("/").split(".")[-1]
+            data = open(data, "rb").read()
+        elif isinstance(data, BytesIO):
+            data = data.getvalue()
+        else:
+            assert isinstance(data, bytes), f"data argument type excepted bytes, got {data.__class__.__name__}"
         
-        payload = {
-            'token': self.token,
+        assert bool(file_format), "file_format argument not specified"
+
+        if file_format.lower() in self.DISABLED:
+            raise InvalidContentType(f'{file_format} is an API blacklisted file extension.')
+
+        req = Request(self._http, 'upload.php', 'POST', data={
+            'token': self.__token,
             'endpoint': 'upload'
-        }
-
-        files = { "image": data }
-
-        req = Request(self._http, 'upload.php', 'POST', data=payload, files=files)
+        }, files={ "bytes": data, "format": file_format })
         try:
             json = await req.json()
         except:
-            raise JsonDecodeException('Failed to decode json from API. Raw content:\n\n' + str(await req.read()))
-        try:
-            return Upload(json['filename'], json['username'], json['url'].replace("\\", ""), datetime.now())
+            raise JsonDecodeException(await req.read())
+        try:U
+            return Upload(json)
         except:
-            if 'error' in json:
-                raise RequestError(json['error'])
-            else:
-                raise RequestError('fatal error uploading.')
+            raise RequestError(json)
 
-    async def get_upload(self, filename: str = None):
-        if filename is None:
-            raise ValueError('filename argument not specified')
-        if type(filename) != str:
-            raise ValueError('filename argument not str, got {}'.format(filename.__class__.__name__))
+    async def get_upload(self, filename: str):
+        if not isinstance(filename, str):
+            raise TypeError(f'filename argument expected str, got {filename.__class__.__name__}')
 
-        payload = {
-            'token': self.token,
+        req = Request(self._http, 'upload.php', 'POST', data={
+            'token': self.__token,
             'endpoint': 'get_upload',
             'filename': filename
-        }
-
-        req = Request(self._http, 'upload.php', 'POST', data=payload)
+        })
         try:
             json = await req.json()
         except:
-            raise JsonDecodeException('Failed to decode json from API. Raw content:\n\n' + await req.read())
+            raise JsonDecodeException(await req.read())
         try:
-            return Upload(json['filename'], json['username'], json['url'].replace("\\", ""), datetime.strptime(json['uploaded_at'], '%B %d %Y %H:%M:%S'))
+            return Upload(json, time=datetime.strptime(json['uploaded_at'], '%B %d %Y %H:%M:%S'))
         except:
-            if 'error' in json:
-                raise RequestError(json['error'])
-            else:
-                raise RequestError('fatal error getting upload info.')
+            raise RequestError(json)
--- iad/request.py
+++ iad/request.py
@@ -1,25 +1,20 @@
-from aiohttp import ClientSession, FormData
+from aiohttp import FormData
 from .ratelimit import RateLimiter
 
-
 class Request:
-    def __init__(self, http: RateLimiter, route: str, method: str, headers: dict = None, data: dict = None, files: dict = None):
-        self.route = 'https://im-a-dev.xyz/'
-        self.route += route
-        
+    def __init__(self, http: RateLimiter, route: str, method: str, headers: dict = {}, data: dict = {}, files: dict = {}):
+        self.route = f'https://im-a-dev.xyz/{route or ""}'
         self.method = method
         self.headers = headers
+        self.http = http
 
-        if data is not None:
+        if data:
             self.data = FormData()
             for k, v in data.items():
                 self.data.add_field(k, v)
 
-            if files is not None:
-                for k, v in files.items():
-                    self.data.add_field(k, v[1], filename=f'file.{v[0]}') # upload with correct file extension
-
-        self.http = http
+            if files:
+                self.data.add_field(k, files['bytes'], filename=f'file.{files["format"]}') # upload with correct file extension
         
     async def json(self):
         if self.method == 'GET':
